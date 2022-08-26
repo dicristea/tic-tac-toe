7eb53e44473fba0ef717ad28e8679e2fc12121df
“dicristea” <dicristea111@gmail.com>
2022-08-26 00:12:00 -0700

    array renders to html

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
-// TO DO
-// Message content in html
+// underscore is for items not to be used outside of its scope
 
 
 // gameBoard is a Module 
 const gameBoard = (() => {
-    // makes an Array with 9 empty slots
-    // underscore is for items not to be used outside of its scope
 
-    const _board = new Array(9);
+    const _board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+    
+    console.log('gameBoard module runs!');
+    const gameField = document.getElementById('game-field');
 
-    const place = (boxNum, sign) => {
+    _board.forEach((box) => {
+        const boxElement = document.createElement('div');
+
+        boxElement.classList.add('box');
+        boxElement.textContent = box;
+        gameField.appendChild(boxElement);
+    })
+
+
+    const place = (boxNum) => {
         if (_board[boxNum] === undefined) {
-            _board[boxNum] = sign;
+            _board[boxNum] = sign
+            // Player.sign;
         };
     };
     
     const isOver = () => {
-        // when winning conditions met, or
+        const _winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        
+        const checkWinner = (field) => {
+            _winConditions.some()
+        };
         // when all spots !== undefined
+
         displayController.setMessage('The game is over.');
     };
 
@@ -27,31 +51,31 @@ const gameBoard = (() => {
         };
     };
 
-    return { setBoxNum, place, isOver, clear };
+    return { place, isOver, clear };
 })();
 
 
 // Player is a factory
-const Player = () => {
-    const name = document.getElementById('name').value;
+const Player = (sign) => {
     //figure out how to make X and O signs
-    const sign = 'X';
 
-    return { name, sign };
+    const playSign = sign;
+
+    return { playSign };
 };
 
+// displayController is a Module 
 const displayController = (() => {
     const _boxElements = document.querySelectorAll('.box');
     const _restartButton = document.getElementById('restart');
     const _messageElement = document.getElementById('message');
- 
-    const _sign = Player.sign;
 
-    _boxElements.forEach((box) =>
+    _boxElements.forEach(box => {
         box.addEventListener("click", (e) => {
-                gameBoard.place(e.target.dataset.index, _sign);
+            console.log('click');
+            gameBoard.place(e.target.dataset.index);
         })
-    );
+    });
 
     _restartButton.addEventListener("click", (e) => {
         gameBoard.clear();
@@ -62,6 +86,23 @@ const displayController = (() => {
         _messageElement.textContent = message;
     };
 
-    return { setMessage };
+})();
+
+const gamePlay = (() => {
+    const playerX = Player("X");
+    const playerO = Player("O");
+    const playNum = 0
+
+
+    
+    playNum % 2 === 1 ? playerX : playerO;
+
+    
+
+
+    if (playNum === 9) {
+        // check winner
+        // set tie message
+    }
 
 })();
